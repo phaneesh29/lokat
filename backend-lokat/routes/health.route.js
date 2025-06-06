55eee3271237b5f48305e5f304eefba5c4e00187
Sreephaneesha <kanugovisreephaneesha@gmail.com>
2025-06-06 22:06:36 +0530

    mistake

--- backend-lokat/routes/health.route.js
+++ backend-lokat/routes/health.route.js
@@ -4,5 +4,6 @@ import { healthcheck } from '../controllers/health.controller.js';
 const router = Router();
 
 router.get('/', healthcheck);
+router.get('/status', healthcheck);
 
 export default router
